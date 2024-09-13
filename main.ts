03d4dd5af376ea87f26f9ef81399e124d8014d58
hvbroboterag <roboterag@wiebracht.de>
2024-09-13 13:23:58 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+	
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
+let Zähler = 0
+basic.forever(function () {
+	
+})
